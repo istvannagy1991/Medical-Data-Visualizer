ebaea80494c2a853303e2143cbea1a5b7400021f
Nagy Istvan <77391657+istvannagy1991@users.noreply.github.com>
2026-03-24 14:34:15 +0100

    normalize data

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -1,3 +1,20 @@
 import pandas as pd
+import matplotlib as plt
+import seaborn as sb
 
 #orvosi adatok megjelenítése
+
+df = pd.read_csv('medical_examination.csv')
+
+#bmi index kiszámítása, 
+bmi = df['weight']/(df['height']*0.01)**2
+#uj oszlop beszúrása, tulsúlyos - 1
+df.loc[bmi>25,'overweight']=1
+df.loc[bmi<=25,'overweight']=0
+
+#adat normalizásás, 0 - jo
+df.loc[df['cholesterol']==1,'cholesterol']=0
+df.loc[df['cholesterol']>1,'cholesterol']=1
+df.loc[df['gluc']==1,'gluc']=0
+df.loc[df['gluc']>1,'gluc']=1
+print(df.head())
